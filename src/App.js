3ef335bd325c155b6040b3813f01c1185d5cdca4
Ongewitter <cedricdemits@gmail.com>
2021-12-02 16:20:42 +0100

    Update app.js to use the components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import InputLabel from './components/InputLabel';
+import TextInput from './components/TextInput';
+import RepositoryTable from './components/repositories/RepositoryTable';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    //TODO: Don't forget to debounce userName changes
+    //TODO: Look into mocking this so github doesn't complain
+    fetch(`https://api.github.com/users/${userName}/repos`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(repositories => {
+        //TODO Figure out what the response is like
+        setRepositories(repositories);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, [userName]);
+
   return (
     <div className="App">
+      <InputLabel>
+        Name:
+        <TextInput name="name" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+      </InputLabel>
+
+      { (repositories.length > 0) ? <RepositoryTable repositories={repositories}/> : '' }
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
