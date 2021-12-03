b0299de287866060d0f01eb849213cbb4af58b85
Ongewitter <cedricdemits@gmail.com>
2021-12-03 16:31:37 +0100

    Look for repositories commits on click

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import InputLabel from './components/InputLabel';
 import TextInput from './components/TextInput';
-import RepositoryTable from './components/repositories/RepositoryTable';
+import RepositoriesTable from './components/repositories/RepositoriesTable';
+import CommitsTable from './components/repositories/CommitsTable';
 
 function App() {
   const BASE_GITHUB_URL = 'https://api.github.com/';
   const [repositories, setRepositories] = useState([]);
+  const [commits, setCommits] = useState([]);
   const [userName, setUserName] = useState('');
 
   function handleSearchClicked() {
@@ -34,6 +36,29 @@ function App() {
       });
   }
 
+  function handleRepositoryClick(repositoryName) {
+    getCommits(userName, repositoryName);
+  }
+
+  function getCommits(userName, repositoryName) {
+    //TODO: Look into mocking this so github doesn't complain
+    fetch(`${BASE_GITHUB_URL}/repos/${userName}/${repositoryName}/commits`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(commits => {
+        //TODO Figure out what the response is like
+        // https://api.github.com/users/Ongewitter/repos
+        setCommits(commits);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
     <div className="App">
       <InputLabel>
@@ -43,6 +68,7 @@ function App() {
       <button onClick={() => handleSearchClicked()}>Search for user</button>
 
       { (repositories.length > 0) ? <RepositoriesTable repositories={repositories} onRepositoryClick={handleRepositoryClick}/> : '' }
+      { (commits.length > 0) ? <CommitsTable commits={commits}/> : '' }
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
