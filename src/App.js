b237a1b56cff8ec3a8dbef8373f3be8aee778689
Ongewitter <cedricdemits@gmail.com>
2021-12-03 16:31:07 +0100

    Use button to search for userName's repositories

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import TextInput from './components/TextInput';
 import RepositoryTable from './components/repositories/RepositoryTable';
 
 function App() {
+  const BASE_GITHUB_URL = 'https://api.github.com/';
   const [repositories, setRepositories] = useState([]);
   const [userName, setUserName] = useState('');
 
-  useEffect(() => {
-    //TODO: Don't forget to debounce userName changes
+  function handleSearchClicked() {
+    getRepositories(userName);
+  }
+
+  function getRepositories(userName) {
+    //TODO: Make this a generic fetch method
     //TODO: Look into mocking this so github doesn't complain
-    fetch(`https://api.github.com/users/${userName}/repos`, {
+    fetch(`${BASE_GITHUB_URL}/users/${userName}/repos`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -21,12 +26,13 @@ function App() {
       .then(response => response.json())
       .then(repositories => {
         //TODO Figure out what the response is like
+        // https://api.github.com/users/Ongewitter/repos
         setRepositories(repositories);
       })
       .catch((error) => {
         console.error('Error:', error);
       });
-  }, [userName]);
+  }
 
   return (
     <div className="App">
@@ -34,8 +40,9 @@ function App() {
         Name:
         <TextInput name="name" value={userName} onChange={(e) => setUserName(e.target.value)}/>
       </InputLabel>
+      <button onClick={() => handleSearchClicked()}>Search for user</button>
 
-      { (repositories.length > 0) ? <RepositoryTable repositories={repositories}/> : '' }
+      { (repositories.length > 0) ? <RepositoriesTable repositories={repositories} onRepositoryClick={handleRepositoryClick}/> : '' }
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
